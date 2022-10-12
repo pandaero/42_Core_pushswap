ed086a455e4963fb5f91ca21bdb90b01ac76e012
pandalaf <pandalaf@student.42wolfsburg.de>
2022-10-12 20:53:06 +0200

    added progress

--- pushswap.h
+++ pushswap.h
@@ -6,7 +6,7 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 10:20:23 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/10/11 17:11:11 by pandalaf         ###   ########.fr       */
+/*   Updated: 2022/10/12 20:50:54 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,5 +34,20 @@ typedef struct s_stack
 	t_node	*bottom;
 	t_node	*top;
 }			t_stack;
+
+//Function checks whether a character is a numeric one.
+int		isnumeric(char ch);
+//Function checks that all the array submembers represent only integers.
+int		isarrint(char **charr, int charrsz);
+//Function checks that array members contain only digits and numeric symbols.
+int		isarrnumeric(char **charr, int charrsz);
+//Function checks whether a string contains a valid signed numeric declaration.
+int		isvalidnum(char *str);
+//Function frees a 2D char array, given its size.
+void	free_charr(char **charr, int charrsz);
+//Function displays error message on standard error.
+int		display_error(void);
+//Function turns a string to a long integer type.
+long	ft_atol(char *str);
 
 #endif
--- src/pushswap.c
+++ src/pushswap.c
@@ -6,32 +6,29 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 11:26:03 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/10/11 21:06:59 by pandalaf         ###   ########.fr       */
+/*   Updated: 2022/10/12 19:58:09 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../pushswap.h"
 #include <stdarg.h>
 
-//Function checks that arguments are valid.
-int	argcheck(char **charr)
+//Function checks that arguments of a string array are valid.
+int	argcheck(char **charr, int charrsz)
 {
-	(check that all are numchars)
-	if (isarrdigit(charr) == 0)
-		return (0);
-	(check that no duplicates)
-	if (isarrdup(charr) == 0)
-		return (0);
-	(check that integers)
-	if (isarrint(charr) == 0)
-		return (0);
+	if (isarrnumeric(charr, charrsz) == 0)
+		return(display_error());
+	if (isarrint(charr, charrsz)) == 0)
+		return (display_error());
+	if (isarrdup(charr, charrsz) == 0)
+		return (display_error());
 	return (1);
 }
 
 //Function sorts a list of numbers using two stacks.
-static void	pushswap(char **charr, t_stack *stacka, t_stack *stackb)
+static void	pushswap(char **charr, int chsz, t_stack *stacka, t_stack *stackb)
 {
-	if (argcheck(charr) == 0)
+	if (argcheck(charr, chsz) == 0)
 		return ;
 	
 }
@@ -49,7 +46,7 @@ int	main(int argc, char **argv)
 		charr = ft_split(argv[1], ' ');
 	else
 		charr = &argv[1];
-	pushswap(charr, &stacka, &stackb);
-	free(charr);
+	pushswap(charr, ft_nwrds(argv[1], ' '), &stacka, &stackb);
+	free_charr(charr, ft_nwrds(argv[1], ' '));
 	return (0);
 }
