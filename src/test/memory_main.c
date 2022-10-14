3292d23f2c54a832e5b9d3a2799668496cc95d78
Pablo Andalaft Tarodo <pandalaf@student.42wolfsburg.de>
2022-10-14 22:47:10 +0200

    added test for free-stack

--- src/test/memory_main.c
+++ src/test/memory_main.c
@@ -2,7 +2,7 @@
 #include "../../pushswap.h"
 #include <stdlib.h>
 
-//* Test for free_charr
+/* Test for free_charr
 int	main(void)
 {
 	char	str[] = "6543 6654 222 1211 33";
@@ -11,5 +11,17 @@ int	main(void)
 	split = ft_split(str, ' ');
 	free_charr(split, ft_nwrds(str, ' '));
 	return (0);
+}
+//*/
+
+/* Test for free_stack
+int	main(void)
+{
+	t_stack	*stack;
+
+	stack = (t_stack *)malloc(sizeof(t_stack));
+	push(5, stack);
+	push(45, stack);
+	free_stack(stack);
 }
 //*/
