0b260e5a2e365d0a556fa5482351fb93338fd085
Pablo Andalaft Tarodo <pandalaf@student.42wolfsburg.de>
2022-10-17 11:33:33 +0200

    completed push swap operations and tests

--- src/operations_pushswap.c
+++ src/operations_pushswap.c
@@ -6,7 +6,7 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 15:28:36 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/10/14 16:11:52 by pandalaf         ###   ########.fr       */
+/*   Updated: 2022/10/17 11:22:54 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,37 +15,49 @@
 //Function pushes the top element of stack b to stack a.
 void	push_a(t_stack *st_a, t_stack *st_b)
 {
-
+	if (st_b->top == 0)
+		return ;
+	push(st_b->top->value, st_a);
+	pop(st_b);
+	ft_printf("pa\n");
 }
 
 //Function pushes the top element of stack a to stack b.
 void	push_b(t_stack *st_a, t_stack *st_b)
 {
-
+	if (st_a->top == 0)
+		return ;
+	push(st_a->top->value, st_b);
+	pop(st_a);
+	ft_printf("pb\n");
 }
 
 //Function swaps the first two elements on a stack a.
 void	swap_a(t_stack *st_a)
 {
-	t_node	*temp;
-	
-	if (st_a->bottom == 0 || (st_a + 1)->bottom == 0)
-		ft_printf("Cannot swap stack with less than 2 elements\n");
-	temp = st_a->bottom->next;
-	st_a->bottom->next = temp;
-	st_a->bottom->next = st_a->bottom;
-	st_a->bottom = temp;
+	if (st_a->top == 0 || st_a->top->prev == 0)
+		return ;
+	swap(st_a);
 	ft_printf("sa\n");
 }
 
 //Function swaps the first two elements on a stack b.
 void	swap_b(t_stack *st_b)
 {
-
+	if (st_b->top == 0 || st_b->top->prev == 0)
+		return ;
+	swap(st_b);
+	ft_printf("sb\n");
 }
 
 //Function swaps the first two elements of each of two stacks a and b.
 void	swap_ab(t_stack *st_a, t_stack *st_b)
 {
-
+	if (st_a->top == 0 || st_b->top == 0)
+		return ;
+	if (st_a->top->prev == 0 || st_b->top->prev == 0)
+		return ;
+	swap(st_a);
+	swap(st_b);
+	ft_printf("ss\n");
 }
--- src/test/ops_pushswap_main.c
+++ src/test/ops_pushswap_main.c
@@ -1,13 +1,119 @@
 // Tests for functions in operations_pushswap.c
-#include "../../pushswap.h"
+#include "test.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-//*
+/* Test for push_a and push_b
 // run: cc ops_pushswap_main.c ../operations_pushswap.c
 int	main(void)
 {
 	t_stack	*stack_a;
+	t_stack *stack_b;
 
+	stack_a = (t_stack *)malloc(sizeof(t_stack));
+	stack_b = (t_stack *)malloc(sizeof(t_stack));
+	printf("Stack \"a\":\n");
+	stack_display(stack_a);
+	printf("Stack \"b\":\n");
+	stack_display(stack_b);
+	printf("---Push a---\n")
+	push_a(stack_a, stack_b);
+	printf("---Push b---"\n);
+	push_b(stack_a, stack_b);
+	printf("Stack \"a\":\n");
+	stack_display(stack_a);
+	printf("Stack \"b\":\n");
+	stack_display(stack_b);
+	push(500, stack_a);
+	push(250, stack_b);
+	printf("Post-Fill Stack \"a\":\n");
+	stack_display(stack_a);
+	printf("Post-Fill Stack \"b\":\n");
+	stack_display(stack_b);
+	printf("---Push a---\n");
+	push_a(stack_a, stack_b);
+	printf("Stack \"a\":\n");
+	stack_display(stack_a);
+	printf("Stack \"b\":\n");
+	stack_display(stack_b);
+	printf("---Push b---\n");
+	push_b(stack_a, stack_b);
+	printf("Stack \"a\":\n");
+	stack_display(stack_a);
+	printf("Stack \"b\":\n");
+	stack_display(stack_b);
+	free_stack(stack_a);
+	free_stack(stack_b);
+	return (0);
+}
+//*/
 
+/* Test for swap_a swap_b and swap_ab
+int	main(void)
+{
+	t_stack	*stack_a;
+	t_stack *stack_b;
+
+	stack_a = (t_stack *)malloc(sizeof(t_stack));
+	stack_b = (t_stack *)malloc(sizeof(t_stack));
+	printf("Stack \"a\":\n");
+	stack_display(stack_a);
+	printf("Stack \"b\":\n");
+	stack_display(stack_b);
+	printf("---Swap a---");
+	swap_a(stack_a);
+	printf("---Swap b---");
+	swap_b(stack_b);
+	printf("Stack \"a\":\n");
+	stack_display(stack_a);
+	printf("Stack \"b\":\n");
+	stack_display(stack_b);
+	push(500, stack_a);
+	push(250, stack_b);
+	printf("Post-One-Fill Stack \"a\":\n");
+	stack_display(stack_a);
+	printf("Post-One-Fill Stack \"b\":\n");
+	stack_display(stack_b);
+	printf("---Swap a---\n");
+	swap_a(stack_a);
+	printf("Stack \"a\":\n");
+	stack_display(stack_a);
+	printf("Stack \"b\":\n");
+	stack_display(stack_b);
+	printf("---Swap b---\n");
+	swap_b(stack_b);
+	printf("Stack \"a\":\n");
+	stack_display(stack_a);
+	printf("Stack \"b\":\n");
+	stack_display(stack_b);
+	push(125, stack_a);
+	push(375, stack_b);
+	push(555, stack_a);
+	push(333, stack_b);
+	printf("Post-Fill Stack \"a\":\n");
+	stack_display(stack_a);
+	printf("Post-Fill Stack \"b\":\n");
+	stack_display(stack_b);
+	printf("---Swap a---\n");
+	swap_a(stack_a);
+	printf("Stack \"a\":\n");
+	stack_display(stack_a);
+	printf("Stack \"b\":\n");
+	stack_display(stack_b);
+	printf("---Swap b---\n");
+	swap_b(stack_b);
+	printf("Stack \"a\":\n");
+	stack_display(stack_a);
+	printf("Stack \"b\":\n");
+	stack_display(stack_b);
+	printf("---Swap both---\n");
+	swap_ab(stack_a, stack_b);
+	printf("Stack \"a\":\n");
+	stack_display(stack_a);
+	printf("Stack \"b\":\n");
+	stack_display(stack_b);
+	free_stack(stack_a);
+	free_stack(stack_b);
 	return (0);
 }
 //*/
