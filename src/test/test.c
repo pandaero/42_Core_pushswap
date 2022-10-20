c606385afc7057300761054645a19ef967bfa7c3
Pablo Andalaft Tarodo <pandalaf@student.42wolfsburg.de>
2022-10-21 01:10:36 +0200

    new testing tool to view nodes

--- src/test/test.c
+++ src/test/test.c
@@ -20,3 +20,37 @@ void	stack_display(t_stack *stack)
 		node = node->prev;
 	}
 }
+
+//Function to display a node and its data.
+void	node_display(t_node *node)
+{
+	printf("Node:\n");
+	printf("Value: %d\n", node->value);
+	printf("Index: %d\n", node->index);
+	printf("Position: %d\n", node->pos);
+	printf("Target Pos: %d\n", node->pos_target);
+	printf("Cost to rot a: %d\n", node->cost_a);
+	printf("Cost to rot b: %d\n", node->cost_b);
+}
+
+//Function prints a whole stack with its nodes data.
+void	stack_display_full(t_stack *stack)
+{
+	t_node	*node;
+
+	node = stack->top;
+	if (node == 0)
+	{
+		printf("Empty\n");
+		return ;
+	}
+	printf("------------------------Top------------------------\n");
+	printf("Val. | Ind. | Pos. | Pos. Tgt. | Cost A | Cost B|\n");
+	while (node != 0)
+	{
+		printf("%5d  %5d  %5d     %5d     %5d    %5d\n", node->value, \
+				node->index, node->pos, node->pos_target, node->cost_a, \
+				node->cost_b);
+		node = node->prev;
+	}
+}
--- src/test/test.h
+++ src/test/test.h
@@ -6,4 +6,8 @@
 
 //Function to display a stack from top to bottom.
 void	stack_display(t_stack *stack);
+//Function to display a node and its data.
+void	node_display(t_node *node);
+//Function prints a whole stack with its nodes data.
+void	stack_display_full(t_stack *stack);
 #endif
