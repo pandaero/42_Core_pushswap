c6e5b4d060403aaac502129a5cf3b79a37df2c99
Pablo Andalaft Tarodo <pandalaf@student.42wolfsburg.de>
2022-10-21 02:25:29 +0200

    added allocation to init function

--- src/node_functions.c
+++ src/node_functions.c
@@ -6,21 +6,29 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 23:35:44 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/10/20 23:37:37 by pandalaf         ###   ########.fr       */
+/*   Updated: 2022/10/21 02:08:40 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../pushswap.h"
+#include <stdlib.h>
 
 //Function initialises a node with only its value.
-void	node_init(int val, t_node *node)
+t_node	*node_init(int val, t_node *node)
 {
+	node = (t_node *)malloc(sizeof(t_node));
+	if (!node)
+	{
+		ft_printf("Malloc error!\n");
+		exit(0);
+	}
 	node->value = val;
 	node->index = 0;
 	node->pos = 0;
 	node->pos_target = 0;
 	node->cost_a = 0;
 	node->cost_b = 0;
+	return (node);
 }
 
 //Function copies values from one node to another.
