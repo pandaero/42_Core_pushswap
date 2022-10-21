d4b1ea594741a24fbec0ede70dad0c30a9171e01
Pablo Andalaft Tarodo <pandalaf@student.42wolfsburg.de>
2022-10-21 06:51:36 +0200

    rearranged functions

--- src/algo_sort.c
+++ src/algo_sort.c
@@ -6,89 +6,13 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 11:48:10 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/10/21 03:42:30 by pandalaf         ###   ########.fr       */
+/*   Updated: 2022/10/21 05:56:54 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../pushswap.h"
 #include <limits.h>
 #include <stdlib.h>
-
-//Function assigns indices to stack elements, in order.
-void	indexing(t_stack *stack)
-{
-	int		ix;
-	int		tmpmin;
-	t_node	*current;
-
-	ix = 1;
-	while (ix <= stack->nodecount)
-	{
-		current = stack->bottom;
-		tmpmin = INT_MAX;
-		while (current != 0)
-		{
-			if (current->index == 0 && current->value < tmpmin)
-				tmpmin = current->value;
-			if (current->next == 0)
-				break ;
-			current = current->next;
-		}
-		while (current != 0)
-		{
-			if (current->index == 0 && current->value == tmpmin)
-				current->index = ix;
-			current = current->prev;
-		}
-		ix++;
-	}
-}
-
-//Function assigns the position of each node.
-void	positioning(t_stack *stack)
-{
-	int		i;
-	t_node	*current;
-
-	current = stack->top;
-	i = 0;
-	while (current != 0)
-	{
-		current->pos = i;
-		current = current->prev;
-		i++;
-	}
-}
-
-//Function calculates the current target position in stack b.
-void	targeting(t_stack *stack_a, t_stack *stack_b)
-{
-	int		pos;
-	t_node	*currenta;
-	t_node	*currentb;
-
-	currentb = stack_b->bottom;
-	while (currentb != 0)
-	{
-		if (currentb->index > index_max(stack_a))
-			currentb->pos_target = stack_index_min(stack_a)->pos;
-		else
-		{
-			currenta = stack_a->bottom;
-			while (currenta != 0)
-			{
-				if (currentb->index == currenta->index - 1)
-					currentb->pos_target = currenta->pos;
-				if (currenta->next == 0)
-					break ;
-				currenta = currenta->next;
-			}
-		}
-		if (currentb->next == 0)
-			break ;
-		currentb = currentb->next;
-	}
-}
 
 //Function pushes all elements to stack b except 3.
 void	initial_push_to_b(t_stack *stack_a, t_stack *stack_b)
--- src/test/algo_sort_main.c
+++ src/test/algo_sort_main.c
@@ -4,50 +4,8 @@
 #include <stdlib.h>
 #include <limits.h>
 
-/* Test for indexing
-// run:
-int	main(void)
-{
-	t_stack *stack;
-	t_node	*node1;
-	t_node	*node2;
-	t_node	*node3;
-	t_node	*node4;
-	t_node	*node5;
-
-	stack = (t_stack *)malloc(sizeof(t_stack));
-	node1 = (t_node *)malloc(sizeof(t_node));
-	node2 = (t_node *)malloc(sizeof(t_node));
-	node3 = (t_node *)malloc(sizeof(t_node));
-	node4 = (t_node *)malloc(sizeof(t_node));
-	node5 = (t_node *)malloc(sizeof(t_node));
-	node_init(100, node1);
-	node_init(50, node2);
-	node_init(150, node3);
-	node_init(INT_MAX, node4);
-	node_init(INT_MIN, node5);
-	push(node1, stack);
-	push(node2, stack);
-	push(node3, stack);
-	push(node4, stack);
-	push(node5, stack);
-	printf("Stack:\n");
-	stack_display_full(stack);
-	printf("Stack nodecnt: %d\n", stack->nodecount);
-	indexing(stack);
-	stack_display_full(stack);
-	free_stack(stack);
-	free(node1);
-	free(node2);
-	free(node3);
-	free(node4);
-	free(node5);
-	return (0);
-}
-//*/
-
 /* Test for initial_push_to_b
-// run:
+// run: cc algo_sort_main.c ../algo_sort.c ../stack_functions.c ../stack_operations.c ../node_functions.c ../operations_pushswap.c ../operations_rotate.c ../operations_reverse_rotate.c ../stack_special.c ../memory.c test.c ../../libft/libft.a
 int	main(void)
 {
 	t_stack *stacka;
@@ -91,7 +49,7 @@ int	main(void)
 //*/
 
 /* Test for sort_three_a
-// run:
+// run: cc algo_sort_main.c ../algo_sort.c ../algo_preparation.c ../algo_execution.c ../stack_functions.c ../stack_operations.c ../node_functions.c ../operations_pushswap.c ../operations_rotate.c ../operations_reverse_rotate.c ../stack_special.c ../stack_search.c ../numbers.c ../memory.c test.c ../../libft/libft.a
 int	main(void)
 {
 	t_stack *stacka;
