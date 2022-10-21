faa7f8d93eae55121cd79f4324d70185c51ac980
Pablo Andalaft Tarodo <pandalaf@student.42wolfsburg.de>
2022-10-21 03:40:35 +0200

    completed targeting function

--- src/algo_sort.c
+++ src/algo_sort.c
@@ -6,12 +6,13 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 11:48:10 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/10/21 02:20:11 by pandalaf         ###   ########.fr       */
+/*   Updated: 2022/10/21 03:39:07 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../pushswap.h"
 #include <limits.h>
+#include <stdlib.h>
 
 //Function assigns indices to stack elements, in order.
 void	indexing(t_stack *stack)
@@ -43,6 +44,52 @@ void	indexing(t_stack *stack)
 	}
 }
 
+//Function assigns the position of each node.
+void	positioning(t_stack *stack)
+{
+	int		i;
+	t_node	*current;
+
+	current = stack->top;
+	i = 0;
+	while (current != 0)
+	{
+		current->pos = i;
+		current = current->prev;
+		i++;
+	}
+}
+
+//Function calculates the current target position in stack b.
+void	targeting(t_stack *stack_a, t_stack *stack_b)
+{
+	int		pos;
+	t_node	*currenta;
+	t_node	*currentb;
+
+	currentb = stack_b->bottom;
+	while (currentb != 0)
+	{
+		if (currentb->index > index_max(stack_a))
+			currentb->pos_target = stack_index_min(stack_a)->pos;
+		else
+		{
+			currenta = stack_a->bottom;
+			while (currenta != 0)
+			{
+				if (currentb->index == currenta->index - 1)
+					currentb->pos_target = currenta->pos;
+				if (currenta->next == 0)
+					break ;
+				currenta = currenta->next;
+			}
+		}
+		if (currentb->next == 0)
+			break ;
+		currentb = currentb->next;
+	}
+}
+
 //Function pushes all elements to stack b except 3.
 void	initial_push_to_b(t_stack *stack_a,t_stack *stack_b)
 {
@@ -66,3 +113,4 @@ void	sort_three_a(t_stack *stack_a)
 	if (node->next->index > node->index)
 		swap_a(stack_a);
 }
+
