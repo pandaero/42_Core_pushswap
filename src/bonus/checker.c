13189de746e9bbf64f28314b5c9b60986ac8cf30
pandalaf <pandalaf@student.42wolfsburg.de>
2022-10-31 22:10:21 +0100

    added skeleton for the checker program

---
+++ src/bonus/checker.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/31 21:21:25 by pandalaf          #+#    #+#             */
+/*   Updated: 2022/10/31 22:09:45 by pandalaf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../checker.h"
+
+//Function handles the main program.
+int	main(int argc, char **argv)
+{
+	//check input is valid and make a stack
+	//read instructions from stdin (make sure valid)
+	//execute instructions
+	//check if stack is sorted (stack b empty)
+	return (0);
+}
