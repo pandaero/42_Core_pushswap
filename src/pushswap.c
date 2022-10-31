ea8db0ab8f2aa08007281047bb2dfc59498244af
Pablo Andalaft Tarodo <pandalaf@student.42wolfsburg.de>
2022-10-31 15:02:50 +0100

    completed the program

--- src/pushswap.c
+++ src/pushswap.c
@@ -6,7 +6,7 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 11:26:03 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/10/21 19:38:45 by pandalaf         ###   ########.fr       */
+/*   Updated: 2022/10/31 15:02:12 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,30 +30,8 @@ int	argcheck(char **charr)
 	return (1);
 }
 
-//Function prints a whole stack with its nodes data.
-void	stack_display_full(t_stack *stack)
-{
-	t_node	*node;
-
-	node = stack->top;
-	if (node == 0)
-	{
-		ft_printf("Empty\n");
-		return ;
-	}
-	ft_printf("------------------------Top------------------------\n");
-	ft_printf("Val. | Ind. | Pos. | Pos. Tgt. | Cost A | Cost B|\n");
-	while (node != 0)
-	{
-		printf("%5d  %5d  %5d     %5d     %5d    %5d\n", node->value, \
-				node->index, node->pos, node->pos_target, node->cost_a, \
-				node->cost_b);
-		node = node->prev;
-	}
-}
-
 //Function runs through the algorithm until stack a is sorted.
-static int algorithm(t_stack *stacka, t_stack *stackb)
+static int	algorithm(t_stack *stacka, t_stack *stackb)
 {
 	while (stack_size(stackb) != 0)
 	{
@@ -61,10 +39,10 @@ static int algorithm(t_stack *stacka, t_stack *stackb)
 		positioning(stackb);
 		targeting(stacka, stackb);
 		costing(stacka, stackb);
-		optimisation_execution(stacka, stackb);
+		perform_operations(stacka, stackb);
 	}
-	//while (stack a not sorted)
-	//	(ra if blah, rra if blah)
+	positioning(stacka);
+	rotation_a(stacka);
 	return (0);
 }
 
@@ -81,18 +59,15 @@ static int	pushswap(char **charr)
 	stack_fill(charr, stacka);
 	indexing(stacka);
 	if (stack_size(stacka) == 2)
-	{
 		swap_a(stacka);
-		return (0);
-	}
 	else if (stack_size(stacka) == 3)
-	{
 		sort_three_a(stacka);
-		return (0);
+	else
+	{
+		initial_push_to_b(stacka, stackb);
+		sort_three_a(stacka);
+		algorithm(stacka, stackb);
 	}
-	initial_push_to_b(stacka, stackb);
-	sort_three_a(stacka);
-	algorithm(stacka, stackb);
 	free_stack(stacka);
 	free_stack(stackb);
 	return (0);
